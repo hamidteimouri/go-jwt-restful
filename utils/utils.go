065b42eaab4dfa12356c432b584e48272b38eb25
Hamid Teimouri <hamidteimouri@outlook.com>
2020-09-22 16:45:49 +0330

    response transformer

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,36 @@
 package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func Message(status bool, message string) map[string]interface{} {
+
+	/* to create response message */
+	return map[string]interface{}{
+		"status":  status,
+		"message": message,
+	}
+
+}
+
+func JsonResponse(w http.ResponseWriter, statusCode int, data map[string]interface{}) {
+	/* adding status code, then encode and send the response */
+	w.WriteHeader(statusCode)
+	err := json.NewEncoder(w).Encode(data)
+
+	if err != nil {
+		fmt.Fprintf(w, "%s", err.Error())
+	}
+}
+
+func ERROR(w http.ResponseWriter, statusCode int, err error) {
+	/* used when we have to give error response */
+	if err != nil {
+		JsonResponse(w, statusCode, Message(false, err.Error()))
+		return
+	}
+	JsonResponse(w, http.StatusBadRequest, nil)
+}

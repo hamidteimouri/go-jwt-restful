74cf60830625da18da0335e36d4e943e19ca4b93
Hamid Teimouri <hamidteimouri@outlook.com>
2020-09-26 18:31:38 +0330

    user controller

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,22 @@
 package controllers
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/hamidteimouri/go-jwt-restful/models"
+	"github.com/hamidteimouri/go-jwt-restful/utils"
+	"net/http"
+)
+
+var CreateUser = func(writer http.ResponseWriter, request *http.Request) {
+	/* decode request body to user struct */
+	user := &models.Token{}
+	err := json.NewDecoder(request.Body).Decode(user)
+	if err != nil {
+		utils.ERROR(writer, http.StatusUnprocessableEntity, errors.New(err.Error()))
+		return
+	}
+
+	/* calling create user function */
+	//response , err := user.Create()
+}

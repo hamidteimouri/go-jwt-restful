7258c74fc0c7f5e1f812c055e196c3f8e7c6cbfa
Hamid Teimouri <hamidteimouri@outlook.com>
2020-09-27 17:28:53 +0330

    base model and user model prepared

--- models/base.go
+++ models/base.go
@@ -1,1 +1,45 @@
 package models
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+var db *gorm.DB
+
+func init() {
+
+	/* setting connection to database */
+	err := godotenv.Load()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	/* getting env variables */
+
+	DbUsername := os.Getenv("DB_USERNAME")
+	DbPassword := os.Getenv("DB_PASSWORD")
+	DbName := os.Getenv("DB_NAME")
+	DbHost := os.Getenv("DB_HOST")
+
+	dbURL := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbUsername, DbName, DbPassword)
+
+	fmt.Println(dbURL)
+
+	conn, err := gorm.Open("mysql", dbURL)
+
+	if err != nil {
+		fmt.Println("connection error", err)
+	}
+	db = conn
+
+	db.Debug().AutoMigrate(&User{})
+}
+
+func getDB() *gorm.DB {
+	/* get gORM db */
+	return db
+}
--- models/user.go
+++ models/user.go
@@ -7,6 +7,8 @@ import (
 	"github.com/hamidteimouri/go-jwt-restful/utils"
 	"github.com/jinzhu/gorm"
 	"golang.org/x/crypto/bcrypt"
+	"os"
+	"strings"
 )
 
 /* struct for token */
@@ -28,9 +30,9 @@ func (user *User) Validate() error {
 		return errors.New("invalid email address")
 	}
 
-	/* check for duplication if email address */
+	/* check for duplication of email address */
 	temp := &User{}
-	err := *gorm.DB.Table("users").Where("email = ?", user.Email).First(temp).Error
+	err := getDB().Table("users").Where("email = ?", user.Email).First(temp).Error
 
 	if err != nil && err != gorm.ErrRecordNotFound {
 		return errors.New("connection error")
@@ -58,8 +60,95 @@ func (user *User) Create() (map[string]interface{}, error) {
 	user.Password = string(hashedPassword)
 
 	/* create the user record */
-	err := *gorm.DB.Create(user).Error
+	err = getDB().Create(user).Error
 	if err != nil {
 		return utils.Message(false, "Failed to create account, connection error"), err
 	}
+
+	/* creating jwt token and set to user struct */
+	tk := &Token{UserId: user.ID}
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+	tokenString, _ := token.SignedString([]byte(os.Getenv("token_password")))
+	user.Token = tokenString
+
+	/* deleting user's password from struct */
+	user.Password = ""
+
+	/* sending back user's account detail as response, without sending its password */
+	response := utils.Message(true, "account created")
+	response["account"] = user
+	return response, nil
+}
+
+func SignIn(email, password string) (map[string]interface{}, error) {
+	user := &User{}
+	err := getDB().Table("users").Where("email = ?", strings.ToLower(email)).First(user).Error
+
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return utils.Message(false, "email address not found"), err
+		}
+		return utils.Message(false, "connection error"), err
+	}
+
+	/* password checking */
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+
+	if err != nil {
+		return utils.Message(false, "invalid login credentials. try again"), errors.New("invalid login credentials")
+	}
+
+	user.Password = ""
+	tk := &Token{UserId: user.ID}
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+	tokenString, _ := token.SignedString([]byte(os.Getenv("token_password")))
+
+	user.Token = tokenString
+
+	/* sending back user's account detail as response, without sending its password */
+	response := utils.Message(true, "logged in")
+	response["user"] = user
+	return response, nil
+}
+
+func DeleteUser(id uint) (map[string]interface{}, error) {
+	/* check for existing user */
+	user := &User{}
+	err := getDB().Table("users").Where("id = ?", id).First(user).Error
+
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return utils.Message(false, "User not found"), err
+		}
+		return utils.Message(false, "Connection error, retry"), err
+	}
+
+	/* deleting user and send response back */
+	getDB().Table("users").Where("id = ?", id).Delete(user)
+	return utils.Message(true, "User Deleted"), nil
+}
+
+func UpdatePassword(id uint, newPassword string) (map[string]interface{}, error) {
+	/* hashing new password and set to database */
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(newPassword), bcrypt.DefaultCost)
+	getDB().Table("users").Where("id = ?", id).Update("password", hashedPassword)
+
+	/* check whether user exist */
+	user := &User{}
+	err := getDB().Table("users").Where("id = ?", id).First(user).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return utils.Message(false, "user not found"), err
+		}
+
+		return utils.Message(false, "Connection error, retry"), err
+	}
+
+	/* check whether updated hashed password same aa new input hashedPassword */
+	if string(hashedPassword) != user.Password {
+		return utils.Message(false, "failed to update password"), errors.New("failed to update password")
+	}
+
+	/* sending response back with message */
+	return utils.Message(true, "Password Updated"), nil
 }
